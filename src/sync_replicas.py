f47be10cd65cdd79acdd5b267b7ad7851b923c37
Scott Sievert <sievert.scott@icloud.com>
2017-10-27 20:25:07 -0500

    ENH: add svd encoding

--- src/sync_replicas.py
+++ src/sync_replicas.py
@@ -2,16 +2,41 @@ import tensorflow as tf
 from tensorflow.python.framework import ops
 
 
-def encode(grads_and_vars):
-    to_log = type(grads_and_vars)
-    for grad, var in grads_and_vars:
-        string = "{} {}".format(grad.get_shape(), var.get_shape())
-        tf.logging.info(string)
+def _4d_to_2d(x):
+    shape = x.get_shape().as_list()
+    ndim = len(shape)
+
+    if ndim == 4:
+        shape = [shape[0]*shape[1], shape[2]*shape[3]]
+    else:
+        tf.logging.warning("#"*30 + "\n" + "CAREFUL! tensor not 4D")
+
+    return tf.reshape(x, shape)
+
+def encode(grads_and_vars, r=2):
+    for i, (grad, var) in enumerate(grads_and_vars):
+        shape = grad.get_shape()
+        if len(shape) == 4:
+            grad = _4d_to_2d(grad)
+        if len(shape) == 2:
+            s, u, v = tf.svd(grad)
+            u = u[:, :r]
+            s = s[:r]
+            v = v[:, :r]
+            coding = {'u': u, 's': s, 'v': v, 'shape': shape}
+            grads_and_vars[i] = (coding, var)
+
     return grads_and_vars
 
 
-def decode(x):
-    return x
+def decode(grads_and_vars):
+    for i, (grad, var) in enumerate(grads_and_vars):
+        if isinstance(grad, dict):
+            tmp = tf.matmul(grad['u'], tf.diag(grad['s']))
+            tmp2 = tf.matmul(tmp, tf.transpose(grad['v']))
+            grad_hat = tf.reshape(tmp2, grad['shape'])
+            grads_and_vars[i] = (grad_hat, var)
+    return grads_and_vars
 
 
 class LowCommSync(tf.train.SyncReplicasOptimizer):
